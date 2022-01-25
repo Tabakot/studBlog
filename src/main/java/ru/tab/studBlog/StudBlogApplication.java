14e9ee8c146d5ccd5b9277543686a24187c3e287
Tabakot <misha-tabach@mail.ru>
2022-01-25 15:16:32 +0400

    Initial commit

---
+++ src/main/java/ru/tab/studBlog/StudBlogApplication.java
@@ -0,0 +1,13 @@
+package ru.tab.studBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudBlogApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ru/tab/studBlog/controller/RegistrationController.java
@@ -0,0 +1,73 @@
+package ru.tab.studBlog.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.tab.studBlog.domain.User;
+import ru.tab.studBlog.service.UserSevice;
+
+import javax.validation.Valid;
+import java.util.Map;
+
+@Controller
+public class RegistrationController {
+    @Autowired
+    private UserSevice userSevice;
+
+    @GetMapping("/registration")
+    public String registration() {
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String addUser(
+            @RequestParam("password2") String passwordConfirm,
+            @Valid User user,
+            BindingResult bindingResult,
+            Model model) {
+        boolean isConfirmEmpty = StringUtils.isEmpty(passwordConfirm);
+        if (isConfirmEmpty) {
+            model.addAttribute("password2Error", "Password confirmation cannot be empty");
+        }
+
+        if (user.getPassword() != null && !user.getPassword().equals(passwordConfirm)) {
+            model.addAttribute("passwordError", "Passwords are different!");
+        }
+
+        if (isConfirmEmpty || bindingResult.hasErrors()) {
+            Map<String, String> errors = ControllerUtils.getErrors(bindingResult);
+
+            model.mergeAttributes(errors);
+
+            return "registration";
+        }
+
+        if (!userSevice.addUser(user)) {
+            model.addAttribute("usernameError", "User exists!");
+            return "registration";
+        }
+
+        return "redirect:/login";
+    }
+
+    @GetMapping("/activate/{code}")
+    public String activate(Model model, @PathVariable String code) {
+        boolean isActivated = userSevice.activateUser(code);
+
+        if (isActivated) {
+            model.addAttribute("messageType", "success");
+            model.addAttribute("message", "User successfully activated");
+        } else {
+            model.addAttribute("messageType", "danger");
+            model.addAttribute("message", "Activation code is not found!");
+        }
+
+        return "login";
+    }
+}
